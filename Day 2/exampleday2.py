29eb8b31d80fe4d8ac5909d993c3161291e18fcd
jmuss07 <jmuss07@charlottesvilleschools.org>
2022-12-02 17:05:29 -0500

    gfrdjvm

--- Day 2/exampleday2.py
+++ Day 2/exampleday2.py
@@ -2,3 +2,12 @@ f = open('Day 1\\example2.txt', 'r')
 lines = f.readlines()
 
 POINTS = {'X':1, 'Y':2, 'Z':3}
+
+max_cal = 0
+new_cal = 0
+old_cal = 0
+current_cal = 0
+
+for line in lines:
+    old_line = line
+    new_line = f.readline().strip()
