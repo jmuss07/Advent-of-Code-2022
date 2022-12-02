808cffe6883e6455ac7b30cd617f9d83ab4c1aaf
jmuss07 <jmuss07@charlottesvilleschools.org>
2022-12-02 11:03:40 -0500

    started say1pt2

--- day1.py
+++ day1.py
@@ -5,11 +5,16 @@ max_cal = 0
 new_cal = 0
 old_cal = 0
 current_cal = 0
+cal2 = 0
+cal3 = 0
+total_cal = 0
+cal_list = []
 
 for line in lines:
     old_line = line
     new_line = f.readline().strip()
     if line.strip() == "":
+        cal_list.append(current_cal)
         current_cal = 0
         new_cal = 0
         old_cal = 0
@@ -22,4 +27,10 @@ for line in lines:
             max_cal = current_cal
         old_cal = current_cal
     line = new_line
+cal_list.sort()
+max_cal = cal_list[-1]
+cal2 = cal_list[-2]
+cal3 = cal_list[-3]
+total_cal = max_cal + cal2 + cal3
 print(max_cal)
+print(total_cal)
