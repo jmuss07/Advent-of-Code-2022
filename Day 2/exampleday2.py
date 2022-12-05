3970bc76def70fca9745788746425e6dd6abfa21
jmuss07 <jmuss07@charlottesvilleschools.org>
2022-12-05 14:09:01 -0500

    worked on day 2

--- Day 2/exampleday2.py
+++ Day 2/exampleday2.py
@@ -1,13 +1,15 @@
-f = open('Day 1\\example2.txt', 'r')
+f = open('Day 2\\inputday2.txt', 'r')
 lines = f.readlines()
 
-POINTS = {'X':1, 'Y':2, 'Z':3}
-
-max_cal = 0
-new_cal = 0
-old_cal = 0
-current_cal = 0
+MATCH_POINTS = {'A X':4, 'B Y':5, 'C Z':7, 'A Y':4, 'A Z':3, 'B X':1, 'B Z':9, 'C X':7, 'C Y':2}
+#x = lose, y = tie, z = win
+#a = rock, b = paper, c = scissors
+#rock = 1, paper = 2, scissors = 3
+match_points = 0
+final_points = 0
 
 for line in lines:
-    old_line = line
-    new_line = f.readline().strip()
+    #new_line = f.readline().strip()
+    match_points = MATCH_POINTS[line.strip()]
+    final_points += match_points
+print(final_points)
